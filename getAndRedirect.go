377d01863f113918ebab9363e3fcdaaeb15ae802
Olivier Hoareau <oha@greenberets.io>
2023-08-20 11:05:07 +0200

    feat add GetAndRedirect()

---
+++ getAndRedirect.go
@@ -0,0 +1,33 @@
+package gohd
+
+import (
+	baseErrors "errors"
+	"github.com/genstackio/goerror"
+	"github.com/genstackio/goerror/errors"
+	"net/http"
+	"strconv"
+)
+
+//goland:noinspection GoUnusedExportedFunction
+func GetAndRedirect(w http.ResponseWriter, req *http.Request, worker func(*http.Request) (string, int, error)) {
+	url, ttl, err := worker(req)
+	if err != nil {
+		goerror.WriteError(w, errors.UnknownDocumentError{
+			Type: "redirect",
+			Err:  baseErrors.New("unknown redirect (" + err.Error() + ")"),
+		})
+		return
+	}
+	if 0 >= len(url) {
+		goerror.WriteError(w, errors.UnknownDocumentError{
+			Type: "redirect",
+			Err:  baseErrors.New("unknown redirect (empty url)"),
+		})
+		return
+	}
+	w.Header().Set("Location", url)
+	if ttl > 0 {
+		w.Header().Set("cache-control", strconv.Itoa(ttl))
+	}
+	w.WriteHeader(http.StatusFound)
+}
