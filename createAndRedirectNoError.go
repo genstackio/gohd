b99112fb93c3bcbd1aa882133a7fde69858c3611
Olivier Hoareau <oha@greenberets.io>
2023-08-30 22:07:35 +0200

    feat: add CreateAndRedirect

--- createAndRedirectNoError.go
+++ createAndRedirectNoError.go
@@ -6,15 +6,15 @@ import (
 )
 
 //goland:noinspection GoUnusedExportedFunction
-func CreateAndRedirectNoError(w http.ResponseWriter, req *http.Request, worker func(*http.Request) (string, int, error), errorUrlFactory func(code int, err error) string) {
+func CreateAndRedirectNoError(w http.ResponseWriter, req *http.Request, worker func(*http.Request) (string, int, error, string), errorUrlFactory func(code int, err error, lang string) string) {
 	CreateAndRedirect(w, req, func(request *http.Request) (string, int, error) {
-		url, ttl, err := worker(request)
+		url, ttl, err, lang := worker(request)
 		if err != nil {
-			url := errorUrlFactory(10104, err)
+			url := errorUrlFactory(10104, err, lang)
 			return url, 0, err
 		}
 		if len(url) == 0 {
-			url = errorUrlFactory(10106, baseErrors.New("empty payment url"))
+			url = errorUrlFactory(10106, baseErrors.New("empty payment url"), lang)
 			return url, 0, err
 		}
 		return url, ttl, nil
