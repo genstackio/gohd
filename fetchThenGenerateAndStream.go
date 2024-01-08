149648e3eb80129a8741e7b8699e5d1575b08ab0
Olivier Hoareau <oha@greenberets.io>
2024-01-08 14:53:59 +0100

    feat: new handlers

--- fetchThenGenerateAndStream.go
+++ fetchThenGenerateAndStream.go
@@ -6,7 +6,7 @@ import (
 )
 
 //goland:noinspection GoUnusedExportedFunction
-func FetchThenGenerateAndStream[T interface{}](w http.ResponseWriter, req *http.Request, fetcher func(req *http.Request) (*T, error), worker func(data *T, w http.ResponseWriter, req *http.Request) error) {
+func FetchThenGenerateAndStream[T interface{}](w http.ResponseWriter, req *http.Request, fetcher func(req *http.Request) (T, error), worker func(data T, w http.ResponseWriter, req *http.Request) error) {
 	data, err := fetcher(req)
 	if nil != err {
 		goerror.WriteError(w, err)
--- fetchThenProcessAndReturn.go
+++ fetchThenProcessAndReturn.go
@@ -8,7 +8,7 @@ import (
 )
 
 //goland:noinspection GoUnusedExportedFunction
-func FetchThenProcessAndReturn[T interface{}, U interface{}](w http.ResponseWriter, req *http.Request, fetcher func(req *http.Request) (*T, error), worker func(data *T, req *http.Request) (*U, error)) {
+func FetchThenProcessAndReturn[T interface{}, U interface{}](w http.ResponseWriter, req *http.Request, fetcher func(req *http.Request) (T, error), worker func(data T, req *http.Request) (U, error)) {
 	data, err := fetcher(req)
 	if nil != err {
 		goerror.WriteError(w, err)
